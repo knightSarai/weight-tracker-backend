8fa68e3ba3434cd9ad0cbfcedf3c11c1f35c9422
knight <knight.sarai.dev@gmail.com>
2021-12-10 22:56:20 +0200

    Add name property for trainee Create WeightMeasurement model Show WeightMeasurement on the admin site

--- trainees/admin.py
+++ trainees/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Trainee
+from .models import Trainee, WeightMeasurement
 
 
 @admin.register(Trainee)
 class TraineeAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(WeightMeasurement)
+class WeightMeasurementAdmin(admin.ModelAdmin):
+    pass
+
--- trainees/models.py
+++ trainees/models.py
@@ -12,3 +12,22 @@ class Trainee(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    @property
+    def name(self):
+        return self.user.username
+
+
+class WeightMeasurement(models.Model):
+    trainee = models.ForeignKey(
+        'trainees.Trainee',
+        related_name='all_trainee_measurements',
+        on_delete=models.DO_NOTHING,
+    )
+    UOM = models.CharField('Unit Of Measurement', max_length=10, default='kg')
+    created_at = models.DateTimeField('Created at', auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField('Updated at', auto_now=True)
+    value = models.CharField('value', max_length=255, null=False)
+
+    def __str__(self):
+        return f'{self.trainee.name}: {self.value}{self.UOM}'
