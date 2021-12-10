0ed942861370c5fa92e6a10a6549c677aedcc027
knight <knight.sarai.dev@gmail.com>
2021-12-11 01:36:38 +0200

    Update permission to just include the owner

--- trainees/permissions.py
+++ trainees/permissions.py
@@ -5,7 +5,4 @@ class WeightMeasurementPermission(BasePermission):
     message = 'Editing/Retrieving measurement is restricted to the trainee or its trainer only.'
 
     def has_object_permission(self, request, view, obj):
-        is_user_trainer = obj.trainee.trainer.user == request.user
-        is_owner = obj.trainee.user == request.user
-
-        return is_user_trainer or is_owner
+        return obj.trainee.user == request.user
