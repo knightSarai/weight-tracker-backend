bb8a1d8e437f90b8fe7b05e52dca11e53db23d77
knight <knight.sarai.dev@gmail.com>
2021-12-12 19:13:00 +0200

    Write the views with a custom logic The views have the same behavior still

--- trainees/views.py
+++ trainees/views.py
@@ -1,33 +1,96 @@
+from django.db import transaction
+from django.http import JsonResponse
 from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_protect
-from rest_framework import permissions, generics
+from django.views.decorators.csrf import ensure_csrf_cookie
+from rest_framework import permissions
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.exceptions import ValidationError
+from rest_framework.views import APIView
 
+from .models import Trainee, WeightMeasurement
 from .permissions import WeightMeasurementPermission
 from .serializers import WeightMeasurementSerializer
 
 
-@method_decorator(csrf_protect, name="dispatch")
-class WeightMeasurementList(generics.ListCreateAPIView, WeightMeasurementPermission):
+class CsrfExemptSessionAuthentication(SessionAuthentication):
+    def enforce_csrf(self, request):
+        return
+
+
+class WeightMeasurementList(APIView):
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
+    permission_classes = [permissions.IsAuthenticated, WeightMeasurementPermission]
+
+    @staticmethod
+    def get(request):
+        user = request.user
+        return JsonResponse(WeightMeasurementSerializer(user.trainee.all_measurements, many=True).data, safe=False)
+
+    def post(self, request):
+        try:
+            data = self.request.data
+            trainee = data.get('trainee')
+            user = request.user
+            if user.trainee.id != trainee:
+                raise ValidationError("User does not match trainee")
+            with transaction.atomic():
+                trainee = Trainee.objects.get(id=trainee)
+                measurement = WeightMeasurement.objects.create(
+                    trainee=trainee,
+                    user_input_date=data.get('user_input_date'),
+                    value=data.get('value')
+                )
+            return JsonResponse({
+                "message": "Measurement created in successfully",
+                "measurement": WeightMeasurementSerializer(measurement).data
+            }, status=201)
+
+        except Exception as e:
+            return JsonResponse({"message": f"{e!r}"}, status=500)
+
+
+@method_decorator(ensure_csrf_cookie, name="dispatch")
+class WeightMeasurementDetail(APIView, WeightMeasurementPermission):
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
     permission_classes = [permissions.IsAuthenticated, WeightMeasurementPermission]
     serializer_class = WeightMeasurementSerializer
 
-    def get_queryset(self):
-        user = self.request.user
-        return user.trainee.all_measurements
+    @staticmethod
+    def get(request, pk):
+        try:
+            measurement = WeightMeasurement.objects.get(id=pk)
+            if request.user.trainee.id != measurement.trainee.id:
+                return JsonResponse({"message": "User does not match trainee"}, status=403)
+            return JsonResponse(WeightMeasurementSerializer(measurement).data, safe=False)
+        except Exception as e:
+            return JsonResponse({"message": f"{e!r}"}, status=500)
 
-    def perform_create(self, serializer):
-        user = self.request.user
-        if user.trainee.id != serializer.validated_data["trainee"].id:
-            raise ValidationError("User does not match trainee")
+    @staticmethod
+    def delete(request, pk):
+        try:
+            measurement = WeightMeasurement.objects.get(id=pk)
+            if request.user.trainee.id != measurement.trainee.id:
+                return JsonResponse({"message": "User does not match trainee"}, status=403)
+            measurement.delete()
+            return JsonResponse({"message": "Measurement deleted successfully", "measurement": {"id": pk}}, status=203)
+        except WeightMeasurement.DoesNotExist:
+            return JsonResponse({"message": "Measurement does not exist"}, status=404)
+        except Exception as e:
+            return JsonResponse({"message": f"{e!r}"}, status=500)
 
-        serializer.save()
-
-
-class WeightMeasurementDetail(generics.RetrieveUpdateDestroyAPIView, WeightMeasurementPermission):
-    permission_classes = [permissions.IsAuthenticated, WeightMeasurementPermission]
-    serializer_class = WeightMeasurementSerializer
-
-    def get_queryset(self):
-        user = self.request.user
-        return user.trainee.all_measurements
+    @staticmethod
+    def put(request, pk):
+        try:
+            measurement = WeightMeasurement.objects.get(id=pk)
+            if request.user.trainee.id != measurement.trainee.id:
+                return JsonResponse({"message": "User does not match trainee"}, status=403)
+            data = request.data
+            measurement.user_input_date = data.get('user_input_date')
+            measurement.value = data.get('value')
+            measurement.save()
+            return JsonResponse({
+                "message": "Measurement updated successfully",
+                "measurement": WeightMeasurementSerializer(measurement).data
+            }, safe=False)
+        except Exception as e:
+            return JsonResponse({"message": f"{e!r}"}, status=500)
